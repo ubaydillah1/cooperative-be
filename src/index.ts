7d6b81018af25982be0ae28b7453a2263ed41874
ubaydillah1 <ubaydillah1737@gmail.com>
2025-09-20 18:59:32 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/test", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
